755c73827e2a7b8c8803e43a44dbc65f9df10982
Samuel Silva <samuelpds77@gmail.com>
2025-11-30 16:02:58 -0300

    DEPOIMENTOS CONTINUADO

---
+++ script.js
@@ -0,0 +1,14 @@
+const dados = [
+    {
+    fala: "Sou um depoimento. Clique aqui para editar e adicionar texto sobre você e seus serviços. Permita que seus clientes recomendem seus serviços e digam a seus amigos o quão profissional você é.",
+    pessoa: "- Gabriela Nascimento"    
+    },
+    {
+    fala: "Sou um depoimento. Clique aqui para editar e adicionar texto sobre você e seus serviços. Permita que seus clientes recomendem seus serviços e digam a seus amigos o quão profissional você é. ",
+    pessoa: "- Amanda Coelho"    
+    },
+    {
+    fala: "Sou um depoimento. Clique aqui para editar e adicionar texto sobre você e seus serviços. Permita que seus clientes recomendem seus serviços e digam a seus amigos o quão profissional você é.",
+    pessoa: "- José Carlos Ernesto"    
+    },
+]
